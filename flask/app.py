bca60ce47d2714b1145af73624c4b11434c3125a
BlazingBurn <49305403+BlazingBurn@users.noreply.github.com>
2023-03-31 14:18:28 +0200

    remove mysql.connector to flask_db

--- flask/app.py
+++ flask/app.py
@@ -1,30 +1,38 @@
 from flask import Flask, render_template
-import mysql.connector
-
-config = {
-   'user': 'root',
-   'password': 'root',
-   'host': 'mysql',
-   'port': '3306',
-   'database': 'sampledb'
-}
-
-mydb = mysql.connector.connect(host='mysql', 
-                               user ="user",
-                               password="user",
-                               database="sampledb")
-
-print("DB connected")
-
-mycursor = mydb.cursor()
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-mycursor.execute("SELECT * FROM fruits")
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_HOST'] = 'mysql'
+app.config['MYSQL_DB'] = 'sampledb'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
-result = mycursor.fetchall()
+mysql = MySQL(app)
 
-mydb.close()
+# config = {
+#    'user': 'root',
+#    'password': 'root',
+#    'host': 'mysql',
+#    'port': '3306',
+#    'database': 'sampledb'
+# }
+
+# mydb = mysql.connector.connect(host='mysql', 
+#                                user ="user",
+#                                password="user",
+#                                database="sampledb")
+
+print("DB connected")
+
+# mycursor = mydb.cursor()
+
+# mycursor.execute("SELECT * FROM fruits")
+
+# result = mycursor.fetchall()
+
+# mydb.close()
 
 @app.route("/")
 def hello_world():
@@ -32,9 +40,15 @@ def hello_world():
 
 @app.route('/fruits')
 def index():
-    mycursor.execute("SELECT * FROM fruits")
-    fruits = mycursor.fetchall()
-    return render_template('fruits.html', fruits=fruits)
+   CS = mysql.connection.cursor()
+   CS.execute('''SELECT * FROM fruits''')
+   Executed_DATA = CS.fetchall()
+   print(Executed_DATA)
+   # return str(Executed_DATA)
+   return render_template('fruits.html', fruits=Executed_DATA)
+   # mycursor.execute("SELECT * FROM fruits")
+   # fruits = mycursor.fetchall()
+   # return render_template('fruits.html', fruits=fruits)
  
 if __name__== "__main__":
 
